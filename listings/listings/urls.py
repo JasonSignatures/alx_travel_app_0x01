b1af0d066c717b276237dc70d14679f2643b8379
Enobong Jackson <106785757+JasonSignatures@users.noreply.github.com>
2025-11-26 22:20:48 +0100

    Update urls.py

--- listings/listings/urls.py
+++ listings/listings/urls.py
@@ -3,11 +3,29 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from listings.views import ListingViewSet, BookingViewSet
 
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Travel App API",
+        default_version="v1",
+        description="API documentation for Listings and Bookings",
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
 router = DefaultRouter()
-router.register(r'listings', ListingViewSet, basename='listing')
-router.register(r'bookings', BookingViewSet, basename='booking')
+router.register(r'listings', ListingViewSet)
+router.register(r'bookings', BookingViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+
+    # Swagger endpoints
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='redoc'),
 ]
