38fa4612c7f4cca6f32021acbb3e1869669f916a
Enobong Jackson <106785757+JasonSignatures@users.noreply.github.com>
2025-11-26 22:19:00 +0100

    Update urls.py

--- listings/listings/urls.py
+++ listings/listings/urls.py
@@ -1,12 +1,13 @@
+from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ListingViewSet, BookingViewSet
+from listings.views import ListingViewSet, BookingViewSet
 
 router = DefaultRouter()
 router.register(r'listings', ListingViewSet, basename='listing')
 router.register(r'bookings', BookingViewSet, basename='booking')
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
-cp -r alx_travel_app_0x00 alx_travel_app_0x01
